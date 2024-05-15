8ed97842aed6beab3b2b22250a86e78855892560
Jakob <jakob.beber@gmail.com>
2024-05-15 21:56:49 +0200

    feat: transform encoded bit output to bytes & comparing size with original

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 
+pdf_file = 'invoicesample.pdf'
+compressed_file = pdf_file+'.compressed.bin'
+
 # find a pdf file of your fav book and read the contents of the file into byte array
-with open('invoicesample.pdf', 'rb') as file:
+with open(pdf_file, 'rb') as file:
     byte_array = file.read()
 
 print(byte_array)
@@ -34,7 +37,26 @@ block_probability = {block : (frequency/num_blocks) for block, frequency in bloc
 # file of the book.
 
 
+# todo: implement shannon-fano algorithm
+
+
+# example of the algorithm output
+encoded_bits = '01010100001110010101010101111011101000011110001010101010101'
+# from shannon-fano algorithm we are going to get a bit string -> transform to byte string
+with open(compressed_file, 'wb') as file:
+    byte_array_encoded = bytearray(int(encoded_bits[i:i+8], 2) for i in range(0, len(encoded_bits), 8))
+    file.write(byte_array_encoded)
+
 
 # Compare the size of the compressed file with the sizes of the same book
 # in some other document formats (DjVu etc.).
+
+import os
+
+file_size_original = os.path.getsize(pdf_file)
+file_size_compressed = os.path.getsize(compressed_file)
+
+print('Original size: '+str(file_size_original)+' bytes')
+print('Compressed size: '+str(file_size_compressed)+' bytes')
+
 
